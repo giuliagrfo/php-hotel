3942e8b17653c1881011c42017923d11fa7a30b0
giuliagrfo <giuliagrifo1@gmail.com>
2022-11-28 16:52:23 +0100

    add table

--- index.php
+++ index.php
@@ -39,6 +39,7 @@ $hotels = [
 
 ];
 
+
 ?>
 
 
@@ -49,11 +50,81 @@ $hotels = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP hotel</title>
+    <style>
+        .description {
+            width: max-content;
+        }
+    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
 <body>
+    <div class="container">
+        <h1 class="text-center m-4">Hotels</h1>
+        <!-- <div class="row">
+            <div class="col bg-primary rounded-3 g-0">
+                <div class="name">Name:</div>
+                <div class="description">Description:</div>
+                <div class="parking">Parking:</div>
+                <div class="vote">Vote:</div>
+                <div class="distance">Distance to center:</div>
+            </div>
 
+            <?php foreach ($hotels as $hotel) : ?>
+                <div class="col rounded-3 g-0">
+                    <div class="name"><?php echo $hotel['name'] ?></div>
+                    <div class="description"><?php echo $hotel['description'] ?></div>
+                    <div class="parking">
+                        <?php
+                        if ($hotel['parking']) {
+                            echo 'This Hotel has parking';
+                        } else {
+                            echo 'No parking';
+                        } ?>
+                    </div>
+                    <div class="vote"><?php echo $hotel['vote'] ?></div>
+                    <div class="distance"><?php echo $hotel['distance_to_center'] ?></div>
+
+                </div>
+            <?php endforeach; ?>
+        </div> -->
+
+        <table class="table table-dark table-md">
+
+            <tbody>
+                <tr>
+                    <th>
+                    <td>
+                        <div class="name">Name:</div>
+                        <div class="description">Description:</div>
+                        <div class="parking">Parking:</div>
+                        <div class="vote">Vote:</div>
+                        <div class="distance">Distance to center:</div>
+                    </td>
+
+                    </th>
+                    <?php foreach ($hotels as $hotel) : ?>
+                        <th>
+                            <div class="name"><?php echo $hotel['name'] ?></div>
+                            <div class="description"><?php echo $hotel['description'] ?></div>
+                            <div class="parking">
+                                <?php
+                                if ($hotel['parking']) {
+                                    echo 'This Hotel has parking';
+                                } else {
+                                    echo 'No parking';
+                                } ?>
+                            </div>
+                            <div class="vote"><?php echo $hotel['vote'] ?></div>
+                            <div class="distance"><?php echo $hotel['distance_to_center'] ?></div>
+
+                        </th>
+                    <?php endforeach; ?>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </body>
 
 </html>
